810f62fd5db3da410e14f3771ddef3b05aec1cda
M.S.Abhinav Chandra <116635275+msabhinavchandra@users.noreply.github.com>
2026-05-24 09:04:49 +0530

    Update Menu-File-Utility-4a.c

--- Menu-File-Utility-4a.c
+++ Menu-File-Utility-4a.c
@@ -1,51 +1,84 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/stat.h>
 
 int main()
 {
-    int ch;
-    char name[50];
-    FILE *fp;
+    int ch, fd;
+    char name[50], data[100];
 
-    printf("1.Create File\n2.Read File\n3.Delete File\n4.Create Directory\n5.Delete Directory\n");
+    printf("1.Create File\n");
+    printf("2.Read File\n");
+    printf("3.Delete File\n");
+    printf("4.Create Directory\n");
+    printf("5.Delete Directory\n");
+
     printf("Enter choice: ");
     scanf("%d", &ch);
 
-    switch (ch)
+    switch(ch)
     {
-    case 1:
-        printf("Enter file name: ");
-        scanf("%s", name);
-        fp = fopen(name, "w");
-        fclose(fp);
-        break;
+        case 1:
+            printf("Enter file name: ");
+            scanf("%s", name);
 
-    case 2:
-        printf("Enter file name: ");
-        scanf("%s", name);
-        fp = fopen(name, "r");
-        if (fp)
-            printf("File opened for reading\n");
-        fclose(fp);
-        break;
+            fd = open(name, O_CREAT | O_RDWR, 0777);
 
-    case 3:
-        printf("Enter file name: ");
-        scanf("%s", name);
-        remove(name);
-        break;
+            if(fd >= 0)
+                printf("File created successfully\n");
 
-    case 4:
-        printf("Enter directory name: ");
-        scanf("%s", name);
-        mkdir(name, 0777);
-        break;
+            close(fd);
+            break;
 
-    case 5:
-        printf("Enter directory name: ");
-        scanf("%s", name);
-        rmdir(name);
-        break;
+        case 2:
+            printf("Enter file name: ");
+            scanf("%s", name);
+
+            fd = open(name, O_RDONLY);
+
+            if(fd >= 0)
+            {
+                read(fd, data, sizeof(data));
+                printf("File Content:\n%s\n", data);
+                close(fd);
+            }
+            else
+            {
+                printf("File not found\n");
+            }
+
+            break;
+
+        case 3:
+            printf("Enter file name: ");
+            scanf("%s", name);
+
+            unlink(name);
+
+            printf("File deleted\n");
+            break;
+
+        case 4:
+            printf("Enter directory name: ");
+            scanf("%s", name);
+
+            mkdir(name, 0777);
+
+            printf("Directory created\n");
+            break;
+
+        case 5:
+            printf("Enter directory name: ");
+            scanf("%s", name);
+
+            rmdir(name);
+
+            printf("Directory deleted\n");
+            break;
+
+        default:
+            printf("Invalid choice\n");
     }
 
     return 0;
